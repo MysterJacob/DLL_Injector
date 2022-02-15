c99af259012269a29ffbc45d8db4719dc83db370
MysterJacob <kubapoloszyk@gmail.com>
2022-02-15 11:38:20 +0100

    Injection WIP

--- DDL Injector/Form1.Designer.cs
+++ DDL Injector/Form1.Designer.cs
@@ -35,9 +35,18 @@ namespace DDL_Injector
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.injectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.pid_label = new System.Windows.Forms.Label();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.githubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.pid_box = new System.Windows.Forms.TextBox();
+            this.lock_on_pid = new System.Windows.Forms.Button();
+            this.Pid_label = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.name_label = new System.Windows.Forms.Label();
+            this.choose_file = new System.Windows.Forms.Button();
+            this.file_path = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -48,20 +57,22 @@ namespace DDL_Injector
             // 
             // inject_button
             // 
-            this.inject_button.Location = new System.Drawing.Point(12, 72);
+            this.inject_button.Location = new System.Drawing.Point(256, 170);
             this.inject_button.Name = "inject_button";
             this.inject_button.Size = new System.Drawing.Size(99, 37);
             this.inject_button.TabIndex = 0;
             this.inject_button.Text = "Inject";
             this.inject_button.UseVisualStyleBackColor = true;
+            this.inject_button.Click += new System.EventHandler(this.inject_button_Click);
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(274, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(602, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -69,8 +80,7 @@ namespace DDL_Injector
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openToolStripMenuItem,
-            this.injectToolStripMenuItem,
-            this.openProcessToolStripMenuItem});
+            this.injectToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
@@ -78,45 +88,127 @@ namespace DDL_Injector
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
             this.openToolStripMenuItem.Text = "Open ";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // injectToolStripMenuItem
             // 
             this.injectToolStripMenuItem.Name = "injectToolStripMenuItem";
-            this.injectToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.injectToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
             this.injectToolStripMenuItem.Text = "Inject";
+            this.injectToolStripMenuItem.Click += new System.EventHandler(this.injectToolStripMenuItem_Click);
             // 
-            // openProcessToolStripMenuItem
+            // helpToolStripMenuItem
             // 
-            this.openProcessToolStripMenuItem.Name = "openProcessToolStripMenuItem";
-            this.openProcessToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.openProcessToolStripMenuItem.Text = "Open process";
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.githubToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
             // 
-            // pid_label
+            // githubToolStripMenuItem
             // 
-            this.pid_label.AutoSize = true;
-            this.pid_label.Location = new System.Drawing.Point(12, 56);
-            this.pid_label.Name = "pid_label";
-            this.pid_label.Size = new System.Drawing.Size(28, 13);
-            this.pid_label.TabIndex = 3;
-            this.pid_label.Text = "PID:";
-            this.pid_label.Click += new System.EventHandler(this.pid_label_Click);
+            this.githubToolStripMenuItem.Name = "githubToolStripMenuItem";
+            this.githubToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.githubToolStripMenuItem.Text = "Github";
+            this.githubToolStripMenuItem.Click += new System.EventHandler(this.githubToolStripMenuItem_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // pid_box
+            // 
+            this.pid_box.Location = new System.Drawing.Point(73, 47);
+            this.pid_box.Name = "pid_box";
+            this.pid_box.Size = new System.Drawing.Size(84, 20);
+            this.pid_box.TabIndex = 5;
+            this.pid_box.TextChanged += new System.EventHandler(this.pid_box_TextChanged);
+            // 
+            // lock_on_pid
+            // 
+            this.lock_on_pid.Location = new System.Drawing.Point(184, 44);
+            this.lock_on_pid.Name = "lock_on_pid";
+            this.lock_on_pid.Size = new System.Drawing.Size(75, 23);
+            this.lock_on_pid.TabIndex = 7;
+            this.lock_on_pid.Text = "Refresh";
+            this.lock_on_pid.UseVisualStyleBackColor = true;
+            this.lock_on_pid.Click += new System.EventHandler(this.lock_on_pid_Click);
+            // 
+            // Pid_label
+            // 
+            this.Pid_label.AutoSize = true;
+            this.Pid_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Pid_label.Location = new System.Drawing.Point(12, 47);
+            this.Pid_label.Name = "Pid_label";
+            this.Pid_label.Size = new System.Drawing.Size(40, 20);
+            this.Pid_label.TabIndex = 8;
+            this.Pid_label.Text = "PID:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(73, 76);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(186, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // name_label
+            // 
+            this.name_label.AutoSize = true;
+            this.name_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.name_label.Location = new System.Drawing.Point(12, 74);
+            this.name_label.Name = "name_label";
+            this.name_label.Size = new System.Drawing.Size(55, 20);
+            this.name_label.TabIndex = 10;
+            this.name_label.Text = "Name:";
+            // 
+            // choose_file
+            // 
+            this.choose_file.Location = new System.Drawing.Point(311, 48);
+            this.choose_file.Name = "choose_file";
+            this.choose_file.Size = new System.Drawing.Size(75, 23);
+            this.choose_file.TabIndex = 11;
+            this.choose_file.Text = "Choose File";
+            this.choose_file.UseVisualStyleBackColor = true;
+            this.choose_file.Click += new System.EventHandler(this.choose_file_Click);
+            // 
+            // file_path
+            // 
+            this.file_path.Enabled = false;
+            this.file_path.Location = new System.Drawing.Point(311, 77);
+            this.file_path.Name = "file_path";
+            this.file_path.Size = new System.Drawing.Size(235, 20);
+            this.file_path.TabIndex = 12;
             // 
             // Form1
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(274, 121);
-            this.Controls.Add(this.pid_label);
+            this.ClientSize = new System.Drawing.Size(602, 219);
+            this.Controls.Add(this.file_path);
+            this.Controls.Add(this.choose_file);
+            this.Controls.Add(this.name_label);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Pid_label);
+            this.Controls.Add(this.lock_on_pid);
+            this.Controls.Add(this.pid_box);
             this.Controls.Add(this.inject_button);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Dll Injector";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,8 +222,16 @@ namespace DDL_Injector
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem injectToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem openProcessToolStripMenuItem;
-        private System.Windows.Forms.Label pid_label;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem githubToolStripMenuItem;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label name_label;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label Pid_label;
+        private System.Windows.Forms.Button lock_on_pid;
+        private System.Windows.Forms.TextBox pid_box;
+        private System.Windows.Forms.TextBox file_path;
+        private System.Windows.Forms.Button choose_file;
     }
 }
 
--- DDL Injector/Form1.cs
+++ DDL Injector/Form1.cs
@@ -7,15 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace DDL_Injector
 {
     public partial class Form1 : Form
     {
         public string FilePath;
+        private List<Process> global_processes;
+        int pid = -1;
         public Form1()
         {
             InitializeComponent();
+            global_processes = new List<Process>();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -41,12 +46,226 @@ namespace DDL_Injector
                 return;
             }
             FilePath = openFileDialog1.FileName;
-            file_path_label.Text = $"File path:{FilePath}";
+            file_path.Text = FilePath;
+            //file_path_label.Text = $"File path:{FilePath}";
         }
 
         private void pid_label_Click(object sender, EventArgs e)
         {
 
+        }
+        private bool lockByPid(int pid)
+        {
+            Process hw = Process.GetProcessById(pid);
+            if (hw == null)
+                return false;
+            return true;
+        }
+        private void githubToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/MysterJacob/DDL_Injector/blob/master/README.md");
+        }
+        private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void openByPIDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pid_box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selected = comboBox1.SelectedItem.ToString();
+            if(selected != null)
+            {
+                pid = global_processes[comboBox1.SelectedIndex].Id;
+                pid_box.Text = pid.ToString();
+            }
+        }
+        private void RefreshList()
+        {
+            Process[] processes = Process.GetProcesses();
+            global_processes.Clear();
+            comboBox1.Items.Clear();
+            global_processes.AddRange(processes);
+            for (int i = 0; i < processes.Length; i++)
+            {
+                comboBox1.Items.Add($"{processes[i].ProcessName}({processes[i].Id})");
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshList();
+        }
+
+        private void lock_on_pid_Click(object sender, EventArgs e)
+        {
+            RefreshList();
+            if (int.TryParse(pid_box.Text, out pid))
+            {
+                try
+                {
+                    Process hw = Process.GetProcessById(pid);
+                    comboBox1.SelectedIndex = comboBox1.Items.IndexOf($"{hw.ProcessName}({hw.Id})");
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("Invalid pid has been provided","INVALID PID",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+
+            }
+        }
+        
+        internal static class kernel32
+        {
+            [Flags]
+            public enum ProcessAccessFlags : uint
+            {
+                All = 0x001F0FFF,
+                Terminate = 0x00000001,
+                CreateThread = 0x00000002,
+                VMOperation = 0x00000008,
+                VMRead = 0x00000010,
+                VMWrite = 0x00000020,
+                DupHandle = 0x00000040,
+                SetInformation = 0x00000200,
+                QueryInformation = 0x00000400,
+                Synchronize = 0x00100000
+            }
+            [Flags]
+            public enum AllocationType
+            {
+                Commit = 0x1000,
+                Reserve = 0x2000,
+                Decommit = 0x4000,
+                Release = 0x8000,
+                Reset = 0x80000,
+                Physical = 0x400000,
+                TopDown = 0x100000,
+                WriteWatch = 0x200000,
+                LargePages = 0x20000000
+            }
+            [Flags]
+            public enum MemoryProtection
+            {
+                Execute = 0x10,
+                ExecuteRead = 0x20,
+                ExecuteReadWrite = 0x40,
+                ExecuteWriteCopy = 0x80,
+                NoAccess = 0x01,
+                ReadOnly = 0x02,
+                ReadWrite = 0x04,
+                WriteCopy = 0x08,
+                GuardModifierflag = 0x100,
+                NoCacheModifierflag = 0x200,
+                WriteCombineModifierflag = 0x400
+            }
+            [DllImport("kernel32.dll")]
+            public static extern IntPtr OpenProcess(ProcessAccessFlags dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, int dwProcessId);
+
+            [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+            public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress,uint dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+
+            [DllImport("kernel32.dll")]
+            public static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress, int dwSize, AllocationType freeType);
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+            public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+            [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+            public static extern IntPtr GetModuleHandle([MarshalAs(UnmanagedType.LPWStr)] in string lpModuleName);
+
+            [DllImport("kernel32.dll")]
+            public static extern IntPtr CreateRemoteThread(IntPtr hProcess,IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+            [DllImport("kernel32.dll")]
+            public static extern Int32 CloseHandle(IntPtr hProcess);
+        }
+
+
+        private void Inject()
+        {
+            if (string.IsNullOrEmpty(FilePath))
+            {
+                MessageBox.Show("No file has been selected", "INVALID PATH", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if(pid == -1)
+            {
+                MessageBox.Show("No process has been selected", "INVALID PID", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //Process hw = Process.GetProcessById(pid);
+            IntPtr hw = kernel32.OpenProcess(kernel32.ProcessAccessFlags.All, false, pid);
+            IntPtr addr = kernel32.VirtualAllocEx(hw, IntPtr.Zero, 0x1000, kernel32.AllocationType.Commit | kernel32.AllocationType.Reserve, kernel32.MemoryProtection.ExecuteReadWrite);
+            if(addr == null)
+            {
+                MessageBox.Show("VirtualAllocEx failed.", "Operation failed.",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                kernel32.CloseHandle(hw);
+                return;
+            }
+            byte[] path = Encoding.ASCII.GetBytes(FilePath);
+            int written;
+            bool ret = kernel32.WriteProcessMemory(hw, addr, path, (uint)path.Length, out written);
+            if (!ret)
+            {
+                MessageBox.Show("WriteProcessMemory failed.", "Operation failed.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                kernel32.VirtualFreeEx(hw, addr, 0x1000, (kernel32.AllocationType)0x8000);
+                kernel32.CloseHandle(hw);
+                return;
+            }
+            IntPtr kernelAddres = kernel32.GetModuleHandle("kernel32.dll");
+            if(kernelAddres == IntPtr.Zero)
+            {
+                MessageBox.Show("Target has no kernel32.dll loaded.", "Operation failed.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                kernel32.VirtualFreeEx(hw, addr, 0x1000, (kernel32.AllocationType)0x8000);
+                kernel32.CloseHandle(hw);
+                return;
+            }
+            IntPtr LoadLibralyA_adress= kernel32.GetProcAddress(
+                kernelAddres,
+                "LoadLibraryA"
+             );
+
+
+            IntPtr thread = kernel32.CreateRemoteThread(hw, IntPtr.Zero, 0, LoadLibralyA_adress, addr ,0, IntPtr.Zero);
+            if(thread == IntPtr.Zero)
+            {
+                MessageBox.Show("Failed to create remote thread.", "Operation failed.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                kernel32.VirtualFreeEx(hw, addr, 0x1000, (kernel32.AllocationType)0x8000);
+                kernel32.CloseHandle(hw);
+                return;
+            }
+            kernel32.CloseHandle(thread);
+
+        }
+        private void inject_button_Click(object sender, EventArgs e)
+        {
+            Inject();
+        }
+
+        private void injectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Inject();
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void choose_file_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
         }
     }
 }
